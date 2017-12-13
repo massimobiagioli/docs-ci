b68a7115c4379bceb9c35f93db843cc45c441bbe
Massimo Biagioli <biagiolimassimo@gmail.com>
2017-12-13 20:07:08 +0100

    sistemato admin layout

--- application/views/admin.php
+++ application/views/admin.php
@@ -17,37 +17,35 @@
                 <div class="card-header">
                     <h3><?php echo $this->lang->line('index_creation'); ?></h3>
                 </div>
-                <div class="card-block mt-2">
-                    <div class="admin-item-content">
-                        <div class="admin-item-icon">
-                            <i class="fa fa-plus-circle fa-4x" aria-hidden="true"></i>
-                        </div> <!-- .admin-item-icon -->
-                        <div class="admin-item-params mt-3">
-                            <form class="form-admin" id="form-admin-create-index" 
-                                class="form-horizontal" 
-                                method="post" 
-                                action="<?= site_url('api/create_index') ?>"
-                                data-update="admin_result admin_error_messages">
+                <div class="admin-item-content card-block mt-2">
+                    <div class="admin-item-icon">
+                        <i class="fa fa-plus-circle fa-4x" aria-hidden="true"></i>
+                    </div> <!-- .admin-item-icon -->
+                    <div class="admin-item-params mt-3">
+                        <form class="form-admin" id="form-admin-create-index" 
+                            class="form-horizontal" 
+                            method="post" 
+                            action="<?= site_url('api/create_index') ?>"
+                            data-update="admin_result admin_error_messages">
 
-                                <!-- Hidden -->
-                                <div>
-                                    <input type="hidden" name="<?php echo $csrf['name']; ?>" value="<?php echo $csrf['hash']; ?>">
-                                </div>
+                            <!-- Hidden -->
+                            <div>
+                                <input type="hidden" name="<?php echo $csrf['name']; ?>" value="<?php echo $csrf['hash']; ?>">
+                            </div>
 
-                                <!-- Index name -->
-                                <div class="form-group">
-                                    <label for="index_name"><?php echo $this->lang->line('index_name'); ?></label>
-                                    <input type="text" name="index_name" class="form-control" required>
-                                </div>            
-                                
-                                <!-- Buttons -->                                
-                                <div class="admin-item-buttons form-group">
-                                    <input type="submit" value="<?php echo $this->lang->line('create_index'); ?>" class="btn btn-primary">
-                                </div>                                
-                            </form>
-                        </div> <!-- .admin-item-params -->
-                    </div> <!-- .admin-item-content -->
-                </div>                
+                            <!-- Index name -->
+                            <div class="form-group">
+                                <label for="index_name"><?php echo $this->lang->line('index_name'); ?></label>
+                                <input type="text" name="index_name" class="form-control" required>
+                            </div>            
+
+                            <!-- Buttons -->                                
+                            <div class="admin-item-buttons form-group">
+                                <input type="submit" value="<?php echo $this->lang->line('create_index'); ?>" class="btn btn-primary">
+                            </div>                                
+                        </form>
+                    </div> <!-- .admin-item-params -->
+                </div> <!-- .admin-item-content -->               
             </div>
         </div> <!-- .col -->
         
@@ -57,37 +55,35 @@
                 <div class="card-header">
                     <h3><?php echo $this->lang->line('index_deletion'); ?></h3>
                 </div>
-                <div class="card-block mt-2">
-                    <div class="admin-item-content">
-                        <div class="admin-item-icon">
-                            <i class="fa fa-minus-circle fa-4x" aria-hidden="true"></i>
-                        </div> <!-- .admin-item-icon -->
-                        <div class="admin-item-params mt-3">
-                            <form class="form-admin" id="form-admin-create-index" 
-                                class="form-horizontal" 
-                                method="post" 
-                                action="<?= site_url('api/delete_index') ?>"
-                                data-update="admin_result admin_error_messages">
+                <div class="admin-item-content card-block mt-2">
+                    <div class="admin-item-icon">
+                        <i class="fa fa-minus-circle fa-4x" aria-hidden="true"></i>
+                    </div> <!-- .admin-item-icon -->
+                    <div class="admin-item-params mt-3">
+                        <form class="form-admin" id="form-admin-create-index" 
+                            class="form-horizontal" 
+                            method="post" 
+                            action="<?= site_url('api/delete_index') ?>"
+                            data-update="admin_result admin_error_messages">
 
-                                <!-- Hidden -->
-                                <div>
-                                    <input type="hidden" name="<?php echo $csrf['name']; ?>" value="<?php echo $csrf['hash']; ?>">
-                                </div>
+                            <!-- Hidden -->
+                            <div>
+                                <input type="hidden" name="<?php echo $csrf['name']; ?>" value="<?php echo $csrf['hash']; ?>">
+                            </div>
 
-                                <!-- Index name -->
-                                <div class="form-group">
-                                    <label for="index_name"><?php echo $this->lang->line('index_name'); ?></label>
-                                    <input type="text" name="index_name" class="form-control" required>
-                                </div>        
-                                
-                                <!-- Buttons -->                                
-                                <div class="admin-item-buttons form-group">
-                                    <input type="submit" value="<?php echo $this->lang->line('delete_index'); ?>" class="btn btn-primary">
-                                </div>                                
-                            </form>
-                        </div> <!-- .admin-item-params -->
-                    </div> <!-- .admin-item-content -->
-                </div>                
+                            <!-- Index name -->
+                            <div class="form-group">
+                                <label for="index_name"><?php echo $this->lang->line('index_name'); ?></label>
+                                <input type="text" name="index_name" class="form-control" required>
+                            </div>        
+
+                            <!-- Buttons -->                                
+                            <div class="admin-item-buttons form-group">
+                                <input type="submit" value="<?php echo $this->lang->line('delete_index'); ?>" class="btn btn-primary">
+                            </div>                                
+                        </form>
+                    </div> <!-- .admin-item-params -->
+                </div> <!-- .admin-item-content -->      
             </div>
         </div> <!-- .col -->
         
@@ -97,31 +93,29 @@
                 <div class="card-header">
                     <h3><?php echo $this->lang->line('migrations'); ?></h3>
                 </div>
-                <div class="card-block mt-2">
-                    <div class="admin-item-content">
-                        <div class="admin-item-icon">
-                            <i class="fa fa-database fa-4x" aria-hidden="true"></i>
-                        </div> <!-- .admin-item-icon -->
-                        <div class="admin-item-params mt-3">
-                            <form class="form-admin" id="form-admin-create-index" 
-                                class="form-horizontal" 
-                                method="post" 
-                                action="<?= site_url('admin/migrate') ?>"
-                                data-update="admin_result admin_error_messages">
+                <div class="admin-item-content card-block mt-2">
+                    <div class="admin-item-icon">
+                        <i class="fa fa-database fa-4x" aria-hidden="true"></i>
+                    </div> <!-- .admin-item-icon -->
+                    <div class="admin-item-params mt-3">
+                        <form class="form-admin" id="form-admin-create-index" 
+                            class="form-horizontal" 
+                            method="post" 
+                            action="<?= site_url('admin/migrate') ?>"
+                            data-update="admin_result admin_error_messages">
 
-                                <!-- Hidden -->
-                                <div>
-                                    <input type="hidden" name="<?php echo $csrf['name']; ?>" value="<?php echo $csrf['hash']; ?>">
-                                </div>
-                                
-                                <!-- Buttons -->                                
-                                <div class="admin-item-buttons form-group">
-                                    <input type="submit" value="<?php echo $this->lang->line('execute'); ?>" class="btn btn-primary">
-                                </div>      
-                            </form>
-                        </div> <!-- .admin-item-params -->
-                    </div> <!-- .admin-item-content -->
-                </div>                
+                            <!-- Hidden -->
+                            <div>
+                                <input type="hidden" name="<?php echo $csrf['name']; ?>" value="<?php echo $csrf['hash']; ?>">
+                            </div>
+
+                            <!-- Buttons -->                                
+                            <div class="admin-item-buttons form-group">
+                                <input type="submit" value="<?php echo $this->lang->line('execute'); ?>" class="btn btn-primary">
+                            </div>      
+                        </form>
+                    </div> <!-- .admin-item-params -->
+                </div> <!-- .admin-item-content -->              
             </div>
         </div> <!-- .col -->
         
