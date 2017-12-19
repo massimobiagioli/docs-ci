41f9f4250d2accea8ad49ceaddf96c0af4a61c2b
Massimo Biagioli <biagiolimassimo@gmail.com>
2017-12-19 20:35:02 +0100

    favicon and meta

--- application/views/fragments/header.php
+++ application/views/fragments/header.php
@@ -8,8 +8,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
         <!-- Required meta tags -->
         <meta charset="utf-8">
+        <meta name="description" content="Docs, a Simple DMS and Storage System">
+        <meta name="keywords" content="HTML5, CSS3, jQuery, Bootstrap, PHP, Codeigniter, ElasticSearch, Heroku, FileStack">
+        <meta http-equiv="X-UA-Compatibible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+        
+        <!-- favicon -->
+        <link rel="shortcut icon" href="<?php echo base_url() . "public/img/favicon.ico"; ?>">
+        
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
         
